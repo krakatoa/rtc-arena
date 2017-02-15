1c5f26148e732f938e950a8d017cf52d6601c7cd
Fernando Alonso <krakatoa1987@gmail.com>
2017-02-15 14:32:18 -0300

    Mesh

---
+++ 6-mesh/signaling.js
@@ -0,0 +1,68 @@
+var socket = io.connect('wss://' + document.domain + ':' + 8943);
+
+socket.on('connect', function(msg) {
+  console.log("connected as " + queryParams().user);
+  socket.emit('join', { user: queryParams().user, room: queryParams().room });
+});
+
+socket.on('disconnect', function(msg) {
+  console.log("disconnected!");
+});
+
+socket.on('message', function(msg) {
+  switch (msg.event) {
+    case 'joined':
+      console.log("user " + msg.user + " joined.");
+      var currentUsers = removeFromList(msg.currentUsers, queryParams().user);
+      console.log("  currentUsers: " + currentUsers);
+      currentUsers.forEach(function(username) {
+        prepareConnectionFor(username);
+      });
+    //default:
+    //  console.log(msg);
+  }
+});
+
+socket.on('new-offer', function(msg) {
+  if (msg.to === queryParams().user) {
+    console.log("received offer from: " + msg.from);
+    var pc = peerConnections[msg.from];
+    pc.setRemoteDescription(msg.sdp).then(function() {
+      pc.addStream(stream);
+      pc.createAnswer().then(
+        function(desc) {
+          pc.setLocalDescription(desc).then(function() {
+            socket.emit('new-answer', { from: queryParams().user, to: msg.from, sdp: desc, room: queryParams().room });
+          }, function(err) {
+            console.log("error setting LocalDescription");
+          });
+        }, function(err) {
+          console.log("error: " + error.toString());
+        }
+      );
+    }, function(err) {
+      console.log("error setting RemoteDescription")
+    });
+  }
+});
+
+socket.on('new-answer', function(msg) {
+  if (msg.to === queryParams().user) {
+    console.log("received answer from: " + msg.from);
+    var pc = peerConnections[msg.from];
+    pc.setRemoteDescription(msg.sdp).then(function() {
+      // setupFinishes = performance.now();
+      // console.log("setup time: " + ((setupFinishes - setupStarts) / 1000));
+    }, function(err) {
+      console.log("error setting remote sdp");
+    });
+  }
+});
+
+function removeFromList(list, rem) {
+  if (list.indexOf(rem) < 0) {
+    return list
+  } else {
+    return list.slice(0, list.indexOf(rem)).concat(list.slice(list.indexOf(rem) + 1, list.length));
+  }
+};
